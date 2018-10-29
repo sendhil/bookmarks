64e64893e6a78fdd7737538fb088a65a5d07f326
Sendhil Panchadsaram <sendhilp@gmail.com [alias]>
2018-10-28 21:03:40 -0700

    Got the urls to return

--- main.go
+++ main.go
@@ -1,30 +1,95 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/buger/jsonparser"
 )
 
 func main() {
 	listBookmarks := flag.Bool("list-bookmarks", true, "Whether to list the bookmarks")
-	bookmarkURL := flag.String("find-bookmark-url", "", "The Bookmark URL to find")
+	bookmark := flag.String("find-bookmark-url", "", "The Bookmark URL to find")
 
 	flag.Parse()
 
-	*listBookmarks = len(*bookmarkURL) == 0
+	*listBookmarks = len(*bookmark) == 0
 
 	if *listBookmarks {
 		outputBookmarks()
-	} else if *bookmarkURL != "" {
-		findURL(*bookmarkURL)
+	} else if *bookmark != "" {
+		*bookmark = strings.TrimSpace(*bookmark)
+		url, err := findURL(*bookmark)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Unable to find Bookmark : '%v'", *bookmark))
+		} else {
+			fmt.Println(url)
+		}
 	}
 }
 
-func findURL(bookmarkURL string) {
-	fmt.Println("Find bookmark url:", bookmarkURL)
+func findURL(bookmark string) (string, error) {
+	fileName := "/home/sendhil/.config/google-chrome/Default/Bookmarks"
+	byteValue, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	nodes := make([][]byte, 0)
+
+	// Initial seed
+	err = jsonparser.ObjectEach(byteValue, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+		if dataType == jsonparser.Object {
+			nodes = append(nodes, value)
+		}
+		return nil
+	}, "roots")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for len(nodes) > 0 {
+		if nodeType, err := jsonparser.GetString(nodes[0], "type"); err == nil {
+			keyVal, err := jsonparser.GetString(nodes[0], "name")
+			if err != nil {
+				fmt.Println("Error parsing:", nodes[0])
+				break
+			}
+
+			if nodeType == "folder" {
+				_, err = jsonparser.ArrayEach(nodes[0], func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+					nodes = append(nodes, value)
+				}, "children")
+
+				if err != nil {
+					fmt.Println("Error parsing children for:", string(nodes[0]))
+				}
+			} else if nodeType == "url" {
+				if strings.ToLower(keyVal) == strings.ToLower(bookmark) {
+					strVal, err := jsonparser.GetString(nodes[0], "url")
+					if err != nil {
+						fmt.Println("Error parsing node : ", string(nodes[0]))
+						return "", err
+					}
+
+					return strVal, nil
+				}
+			} else {
+				fmt.Println("Unknown node type: ", nodeType)
+			}
+		} else {
+			fmt.Println("Unable to parse node : ", string(nodes[0]))
+			fmt.Println("err:", err)
+		}
+
+		nodes = append(nodes[1:])
+	}
+
+	return "", errors.New("Not Found")
 }
 
 func outputBookmarks() {
