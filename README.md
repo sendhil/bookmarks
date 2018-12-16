c90226e36147a02723280414b02826aabbce70c4
Sendhil Panchadsaram <sendhilp@gmail.com>
2018-12-16 15:32:29 -0800

    Some cleanup

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Bookmarks
 
-This project is meant to be a helper for Rofi to allow the quick opening of chrome bookmarks. There's an example script in examples/ that can be used with Rofi.
+This project is meant to be a helper for Rofi to enable quick opening of Chrome Bookmarks. There's an example script in examples/ that can be used with Rofi.
--- examples/rofi-chrome-bookmarks.sh
+++ examples/rofi-chrome-bookmarks.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-# Rofi dmenu
-# rofi_dmenu(){
-#   rofi -dmenu -p "Bookmarks"
-# }
-
-
-# rofi_dmenu(){
-#   rofi -dmenu -i -matching fuzzy -only-match -p "Select a repository > "\
-#     -location 0 -bg "#F8F8FF" -fg "#000000" -hlbg "#ECECEC" -hlfg "#0366d6"
-# }
-
 rofi_dmenu(){
   rofi -dmenu -i -matching fuzzy -p "Select bookmark"\
     -location 0 -bg "#F8F8FF" -fg "#000000" -hlbg "#ECECEC" -hlfg "#0366d6"
