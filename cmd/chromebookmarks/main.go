c5e251e65c218479de8016c2a72fd1806b6a88ee
Sendhil Panchadsaram <sendhilp@gmail.com [alias]>
2019-04-06 17:56:16 -0700

    Adding option for filename

--- cmd/chromebookmarks/main.go
+++ cmd/chromebookmarks/main.go
@@ -12,6 +12,7 @@ func main() {
 	listBookmarks := flag.Bool("list-bookmarks", true, "Whether to list the bookmarks")
 	bookmark := flag.String("find-bookmark-url", "", "The Bookmark URL to find")
 	outputJSON := flag.Bool("output-json", false, "Whether to Output Chrome Bookmarks as JSON")
+	fileName := flag.String("json-file-name", "home.json", "The JSON File Name for Bookmarks")
 
 	flag.Parse()
 
@@ -20,10 +21,10 @@ func main() {
 	if *outputJSON {
 		parsers.OutputChromeBookmarks(true)
 	} else if *listBookmarks {
-		parsers.OutputJSONBookmarks("home.json", false)
+		parsers.OutputJSONBookmarks(*fileName, false)
 	} else if *bookmark != "" {
 		*bookmark = strings.TrimSpace(*bookmark)
-		url, err := parsers.FindJSONBookmarkURL(*bookmark, "home.json")
+		url, err := parsers.FindJSONBookmarkURL(*bookmark, *fileName)
 		if err != nil {
 			fmt.Println(fmt.Sprintf("Unable to find Bookmark : '%v'", *bookmark))
 		} else {
